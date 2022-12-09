346807eb06e134781a2b9d5d665e554a363fa2dd
Joao Teles <joaojoseteles@gmail.com>
2022-12-09 20:20:09 -0300

    :tada: Commit inicial

---
+++ mobile/src/screens/Signin.tsx
@@ -0,0 +1,16 @@
+import { Center, Text } from "native-base";
+
+export function SignIn() {
+  return (
+    <Center flex={1} bgColor="gray.900">
+      <Text
+        color="gray.200"
+        textAlign="center"
+        fontSize={24}
+        fontFamily="heading"
+      >
+        Hello React Native!
+      </Text>
+    </Center>
+  );
+}
